3c4010cf67a0a92f9572cfa52316c9e97f952c75
ed <s@ocv.me>
2020-12-20 02:26:48 +0100

    add sfx based on copyparty

---
+++ scripts/make-sfx.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+set -e
+echo
+
+
+# optional args:
+#
+# `clean` uses files from git (everything except web/deps),
+#   so local changes won't affect the produced sfx
+
+
+# differences from copyparty sfx:
+# - no jinja stuff
+# - no python scanning stuff
+# - no unexpand (breaks heredoc)
+# - but does share/docs instead
+# - site-packages subfolder
+
+
+name=r0c
+
+[ -e $name/__main__.py ] || cd ..
+[ -e $name/__main__.py ] ||
+{
+	printf "run me from within the project root folder\n\n"
+	exit 1
+}
+
+resources=(
+	docs/*.md
+)
+
+command -v gtar  >/dev/null &&
+command -v gfind >/dev/null && {
+	tar()  { gtar  "$@"; }
+	sed()  { gsed  "$@"; }
+	find() { gfind "$@"; }
+	sort() { gsort "$@"; }
+}
+
+while [ ! -z "$1" ]; do
+	[ "$1" = clean  ] && clean=1  && shift && continue
+	break
+done
+
+tmv() {
+	touch -r "$1" t
+	mv t "$1"
+}
+
+rm -rf sfx/*
+mkdir -p sfx build
+cd sfx
+
+# msys2 tar is bad, make the best of it
+echo collecting source
+[ $clean ] && {
+	(cd .. && git archive master >tar) && tar -xf ../tar $name "${resources[@]}"
+}
+[ $clean ] || {
+	(cd .. && tar -cf tar $name "${resources[@]}") && tar -xf ../tar
+}
+rm -f ../tar
+
+ver=
+git describe --tags >/dev/null 2>/dev/null && {
+	git_ver="$(git describe --tags)";  # v0.5.5-2-gb164aa0
+	ver="$(printf '%s\n' "$git_ver" | sed -r 's/^v//; s/-g?/./g')";
+	t_ver=
+
+	printf '%s\n' "$git_ver" | grep -qE '^v[0-9\.]+$' && {
+		# short format (exact version number)
+		t_ver="$(printf '%s\n' "$ver" | sed -r 's/\./, /g')";
+	}
+
+	printf '%s\n' "$git_ver" | grep -qE '^v[0-9\.]+-[0-9]+-g[0-9a-f]+$' && {
+		# long format (unreleased commit)
+		t_ver="$(printf '%s\n' "$ver" | sed -r 's/\./, /g; s/(.*) (.*)/\1 "\2"/')"
+	}
+
+	[ -z "$t_ver" ] && {
+		printf 'unexpected git version format: [%s]\n' "$git_ver"
+		exit 1
+	}
+
+	dt="$(git log -1 --format=%cd --date=format:'%Y,%m,%d' | sed -E 's/,0?/, /g')"
+	printf 'git %3s: \033[36m%s\033[0m\n' ver "$ver" dt "$dt"
+	sed -ri '
+		s/^(VERSION =)(.*)/#\1\2\n\1 ('"$t_ver"')/;
+		s/^(S_VERSION =)(.*)/#\1\2\n\1 "'"$ver"'"/;
+		s/^(BUILD_DT =)(.*)/#\1\2\n\1 ('"$dt"')/;
+	' $name/__version__.py
+}
+
+[ -z "$ver" ] && 
+	ver="$(awk '/^VERSION *= \(/ {
+		gsub(/[^0-9,]/,""); gsub(/,/,"."); print; exit}' < $name/__version__.py)"
+
+ts=$(date -u +%s)
+hts=$(date -u +%Y-%m%d-%H%M%S) # --date=@$ts (thx osx)
+
+mkdir -p ../dist
+sfx_out=../dist/$name
+
+echo cleanup
+find .. -name '*.pyc' -delete
+find .. -name __pycache__ -delete
+
+# especially prevent osx from leaking your lan ip (wtf apple)
+find .. -type f \( -name .DS_Store -or -name ._.DS_Store \) -delete
+find .. -type f -name ._\* | while IFS= read -r f; do cmp <(printf '\x00\x05\x16') <(head -c 3 -- "$f") && rm -f -- "$f"; done
+
+# disable password check (TODO should be a r0c option probably)
+f=$name/__main__.py
+awk '/change the ADMIN_PWD/{o=1} o&&/return False$/{sub(/False/,"True");o=0} 1' <$f >t
+tmv "$f"
+
+# r0c needs the docs here
+mkdir -p share/doc/r0c/
+mv docs share/doc/r0c/help
+mkdir site-packages
+mv $name share site-packages
+
+echo creating tar
+args=(--owner=1000 --group=1000)
+[ "$OSTYPE" = msys ] &&
+	args=()
+
+tar -cf tar "${args[@]}" --numeric-owner site-packages
+
+echo compressing tar
+# detect best level; bzip2 -7 is usually better than -9
+for n in {2..9}; do cp tar t.$n; bzip2 -$n t.$n & done; wait; mv -v $(ls -1S t.*.bz2 | tail -n 1) tar.bz2
+rm t.*
+
+echo creating sfx
+python ../scripts/sfx.py --sfx-make tar.bz2 r0c $ver $ts
+mv sfx.out $sfx_out.py
+chmod 755 $sfx_out.*
+
+printf "done:\n"
+printf "  %s\n" "$(realpath $sfx_out)."py
+# rm -rf *
+
+# tar -tvf ../sfx/tar | sed -r 's/(.* ....-..-.. ..:.. )(.*)/\2 `` \1/' | sort | sed -r 's/(.*) `` (.*)/\2 \1/'| less
+# for n in {1..9}; do tar -tf tar | grep -vE '/$' | sed -r 's/(.*)\.(.*)/\2.\1/' | sort | sed -r 's/([^\.]+)\.(.*)/\2.\1/' | tar -cT- | bzip2 -c$n | wc -c; done 
---
+++ scripts/sfx.py
@@ -0,0 +1,457 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function, unicode_literals
+
+import os, sys, time, shutil, signal, tarfile, hashlib, platform, tempfile
+import subprocess as sp
+
+"""
+run me with any version of python, i will unpack and run $NAME
+
+(but please don't edit this file with a text editor
+  since that would probably corrupt the binary stuff at the end)
+
+there's zero binaries! just plaintext python scripts all the way down
+  so you can easily unpack the archive and inspect it for shady stuff
+
+the archive data is attached after the b"\n# eof\n" archive marker,
+  b"\n#n" decodes to b"\n"
+  b"\n#r" decodes to b"\r"
+  b"\n# " decodes to b""
+"""
+
+# set by make-sfx.sh
+VER = None
+SIZE = None
+CKSUM = None
+STAMP = None
+
+NAME = "r0c"
+PY2 = sys.version_info[0] == 2
+WINDOWS = platform.system() == "Windows"
+sys.dont_write_bytecode = True
+me = os.path.abspath(os.path.realpath(__file__))
+cpp = None
+
+
+def eprint(*args, **kwargs):
+    kwargs["file"] = sys.stderr
+    print(*args, **kwargs)
+
+
+def msg(*args, **kwargs):
+    if args:
+        args = ["[SFX]", args[0]] + list(args[1:])
+
+    eprint(*args, **kwargs)
+
+
+# skip 1
+
+
+def testptn1():
+    """test: creates a test-pattern for encode()"""
+    import struct
+
+    buf = b""
+    for c in range(256):
+        buf += struct.pack("B", c)
+
+    yield buf
+
+
+def testptn2():
+    import struct
+
+    for a in range(256):
+        if a % 16 == 0:
+            msg(a)
+
+        for b in range(256):
+            buf = b""
+            for c in range(256):
+                buf += struct.pack("BBBB", a, b, c, b)
+            yield buf
+
+
+def testptn3():
+    with open("C:/Users/ed/Downloads/python-3.8.1-amd64.exe", "rb", 512 * 1024) as f:
+        while True:
+            buf = f.read(512 * 1024)
+            if not buf:
+                break
+
+            yield buf
+
+
+testptn = testptn2
+
+
+def testchk(cdata):
+    """test: verifies that `data` yields testptn"""
+    import struct
+
+    cbuf = b""
+    mbuf = b""
+    checked = 0
+    t0 = time.time()
+    mdata = testptn()
+    while True:
+        if not mbuf:
+            try:
+                mbuf += next(mdata)
+            except:
+                break
+
+        if not cbuf:
+            try:
+                cbuf += next(cdata)
+            except:
+                expect = mbuf[:8]
+                expect = "".join(
+                    " {:02x}".format(x)
+                    for x in struct.unpack("B" * len(expect), expect)
+                )
+                raise Exception(
+                    "truncated at {}, expected{}".format(checked + len(cbuf), expect)
+                )
+
+        ncmp = min(len(cbuf), len(mbuf))
+        # msg("checking {:x}H bytes, {:x}H ok so far".format(ncmp, checked))
+        for n in range(ncmp):
+            checked += 1
+            if cbuf[n] != mbuf[n]:
+                expect = mbuf[n : n + 8]
+                expect = "".join(
+                    " {:02x}".format(x)
+                    for x in struct.unpack("B" * len(expect), expect)
+                )
+                cc = struct.unpack(b"B", cbuf[n : n + 1])[0]
+                raise Exception(
+                    "byte {:x}H bad, got {:02x}, expected{}".format(checked, cc, expect)
+                )
+
+        cbuf = cbuf[ncmp:]
+        mbuf = mbuf[ncmp:]
+
+    td = time.time() - t0
+    txt = "all {}d bytes OK in {:.3f} sec, {:.3f} MB/s".format(
+        checked, td, (checked / (1024 * 1024.0)) / td
+    )
+    msg(txt)
+
+
+def encode(data, size, cksum, name, ver, ts):
+    """creates a new sfx; `data` should yield bufs to attach"""
+    nin = 0
+    nout = 0
+    skip = False
+    with open(me, "rb") as fi:
+        unpk = ""
+        src = fi.read().replace(b"\r", b"").rstrip(b"\n").decode("utf-8")
+        src = src.replace("$NAME", NAME)
+        for ln in src.split("\n"):
+            if ln.endswith("# skip 0"):
+                skip = False
+                continue
+
+            if ln.endswith("# skip 1") or skip:
+                skip = True
+                continue
+
+            unpk += ln + "\n"
+
+        for k, v in [
+            ["VER", '"' + ver + '"'],
+            ["SIZE", size],
+            ["CKSUM", '"' + cksum + '"'],
+            ["STAMP", ts],
+        ]:
+            v1 = "\n{} = None\n".format(k)
+            v2 = "\n{} = {}\n".format(k, v)
+            unpk = unpk.replace(v1, v2)
+
+        unpk = unpk.replace("\n    ", "\n\t")
+        for _ in range(16):
+            unpk = unpk.replace("\t    ", "\t\t")
+
+    with open("sfx.out", "wb") as f:
+        f.write(unpk.encode("utf-8") + b"\n\n# eof\n# ")
+        for buf in data:
+            ebuf = buf.replace(b"\n", b"\n#n").replace(b"\r", b"\n#r")
+            f.write(ebuf)
+            nin += len(buf)
+            nout += len(ebuf)
+
+    msg("wrote {:x}H bytes ({:x}H after encode)".format(nin, nout))
+
+
+def makesfx(tar_src, name, ver, ts):
+    sz = os.path.getsize(tar_src)
+    cksum = hashfile(tar_src)
+    encode(yieldfile(tar_src), sz, cksum, name, ver, ts)
+
+
+# skip 0
+
+
+def u8(gen):
+    try:
+        for s in gen:
+            yield s.decode("utf-8", "ignore")
+    except:
+        yield s
+        for s in gen:
+            yield s
+
+
+def yieldfile(fn):
+    with open(fn, "rb") as f:
+        for block in iter(lambda: f.read(64 * 1024), b""):
+            yield block
+
+
+def hashfile(fn):
+    hasher = hashlib.md5()
+    for block in yieldfile(fn):
+        hasher.update(block)
+
+    return hasher.hexdigest()
+
+
+def unpack():
+    """unpacks the tar yielded by `data`"""
+    name = "pe-" + NAME
+    tag = "v" + str(STAMP)
+    withpid = "{}.{}".format(name, os.getpid())
+    top = tempfile.gettempdir()
+    final = os.path.join(top, name)
+    mine = os.path.join(top, withpid)
+    tar = os.path.join(mine, "tar")
+
+    try:
+        if tag in os.listdir(final):
+            msg("found early")
+            return final
+    except:
+        pass
+
+    nwrite = 0
+    os.mkdir(mine)
+    with open(tar, "wb") as f:
+        for buf in get_payload():
+            nwrite += len(buf)
+            f.write(buf)
+
+    if nwrite != SIZE:
+        t = "\n\n  bad file:\n    expected {} bytes, got {}\n".format(SIZE, nwrite)
+        raise Exception(t)
+
+    cksum = hashfile(tar)
+    if cksum != CKSUM:
+        t = "\n\n  bad file:\n    {} expected,\n    {} obtained\n".format(CKSUM, cksum)
+        raise Exception(t)
+
+    with tarfile.open(tar, "r:bz2") as tf:
+        tf.extractall(mine)
+
+    os.remove(tar)
+
+    with open(os.path.join(mine, tag), "wb") as f:
+        f.write(b"h\n")
+
+    try:
+        if tag in os.listdir(final):
+            msg("found late")
+            return final
+    except:
+        pass
+
+    try:
+        if os.path.islink(final):
+            os.remove(final)
+        else:
+            shutil.rmtree(final)
+    except:
+        pass
+
+    try:
+        os.symlink(mine, final)
+    except:
+        try:
+            os.rename(mine, final)
+        except:
+            msg("reloc fail,", mine)
+            return mine
+
+    for fn in u8(os.listdir(top)):
+        if fn.startswith(name) and fn not in [name, withpid]:
+            try:
+                old = os.path.join(top, fn)
+                if time.time() - os.path.getmtime(old) > 10:
+                    shutil.rmtree(old)
+            except:
+                pass
+
+    return final
+
+
+def get_payload():
+    """yields the binary data attached to script"""
+    with open(me, "rb") as f:
+        ptn = b"\n# eof\n# "
+        buf = b""
+        for n in range(64):
+            buf += f.read(4096)
+            ofs = buf.find(ptn)
+            if ofs >= 0:
+                break
+
+        if ofs < 0:
+            raise Exception("could not find archive marker")
+
+        # start reading from the final b"\n"
+        fpos = ofs + len(ptn) - 3
+        # msg("tar found at", fpos)
+        f.seek(fpos)
+        dpos = 0
+        leftovers = b""
+        while True:
+            rbuf = f.read(1024 * 32)
+            if rbuf:
+                buf = leftovers + rbuf
+                ofs = buf.rfind(b"\n")
+                if len(buf) <= 4:
+                    leftovers = buf
+                    continue
+
+                if ofs >= len(buf) - 4:
+                    leftovers = buf[ofs:]
+                    buf = buf[:ofs]
+                else:
+                    leftovers = b"\n# "
+            else:
+                buf = leftovers
+
+            fpos += len(buf) + 1
+            buf = (
+                buf.replace(b"\n# ", b"")
+                .replace(b"\n#r", b"\r")
+                .replace(b"\n#n", b"\n")
+            )
+            dpos += len(buf) - 1
+
+            yield buf
+
+            if not rbuf:
+                break
+
+
+def confirm():
+    msg()
+    msg("*** hit enter to exit ***")
+    try:
+        raw_input() if PY2 else input()
+    except:
+        pass
+
+
+def run(tmp, py):
+    global cpp
+
+    msg("OK")
+    msg("will use:", py)
+    msg("bound to:", tmp)
+
+    # "systemd-tmpfiles-clean.timer"?? HOW do you even come up with this shit
+    try:
+        import fcntl
+
+        fd = os.open(tmp, os.O_RDONLY)
+        fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        tmp = os.readlink(tmp)  # can't flock a symlink, even with O_NOFOLLOW
+    except:
+        pass
+
+    fp_py = os.path.join(tmp, "py")
+    try:
+        with open(fp_py, "wb") as f:
+            f.write(py.encode("utf-8") + b"\n")
+    except:
+        pass
+
+    args = list(sys.argv[1:])
+    if not args:
+        if WINDOWS:
+            args = [23, 531]
+        else:
+            args = [2323, 1531]
+
+    cmd = 'import sys, runpy; sys.path.insert(0, r"{0}{1}site-packages"); runpy.run_module("$NAME", run_name="__main__")'.format(
+        tmp, os.sep
+    )
+    cmd = [py, "-c", cmd] + args
+
+    msg("\n", cmd, "\n")
+    cpp = sp.Popen(str(x) for x in cmd)
+    try:
+        cpp.wait()
+    except:
+        cpp.wait()
+
+    if cpp.returncode != 0:
+        confirm()
+
+    sys.exit(cpp.returncode)
+
+
+def bye(sig, frame):
+    if cpp is not None:
+        cpp.terminate()
+
+
+def main():
+    sysver = str(sys.version).replace("\n", "\n" + " " * 18)
+    pktime = time.strftime("%Y-%m-%d, %H:%M:%S", time.gmtime(STAMP))
+    os.system("")
+    msg()
+    msg("   this is: $NAME", VER)
+    msg(" packed at:", pktime, "UTC,", STAMP)
+    msg("archive is:", me)
+    msg("python bin:", sys.executable)
+    msg("python ver:", platform.python_implementation(), sysver)
+    msg()
+
+    arg = ""
+    try:
+        arg = sys.argv[1]
+    except:
+        pass
+
+    # skip 1
+
+    if arg == "--sfx-testgen":
+        return encode(testptn(), 1, "x", "x", 1)
+
+    if arg == "--sfx-testchk":
+        return testchk(get_payload())
+
+    if arg == "--sfx-make":
+        tar, name, ver, ts = sys.argv[2:]
+        return makesfx(tar, name, ver, ts)
+
+    # skip 0
+
+    signal.signal(signal.SIGTERM, bye)
+
+    tmp = unpack()
+    return run(tmp, sys.executable)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# skip 1
+# python sfx.py --sfx-testgen && python test.py --sfx-testchk
+# c:\Python27\python.exe sfx.py --sfx-testgen && c:\Python27\python.exe test.py --sfx-testchk
